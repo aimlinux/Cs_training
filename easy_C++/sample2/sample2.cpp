51ce428d83ce6e3420da3606af526d2c135b3382
aimlinux <1k8aioon@gmail.com>
2026-05-19 10:01:41 +0900

    create sample2

---
+++ easy_C++/sample2/sample2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+    cout << 1 << 2 << 3 << "\n" 
+        << 4 << 5 << 6 << "\n" 
+        << 7 << 8 << 9 << "\n";
+        //通常は続けて出力される
+
+    cout << endl; // 改行
+    return 0;
+}
