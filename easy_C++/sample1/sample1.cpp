fef17fcd449b2cab0932f081544722e8d78b575f
aimlinux <1k8aioon@gmail.com>
2026-05-19 09:52:48 +0900

    create C+++.

---
+++ easy_C++/sample1/sample1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "ようこそ、C++の世界へ！\n";
+    cout << "C++は、強力なプログラミング言語です。\n";  
+    cout << "これから一緒に学んでいきましょう！\n";
+
+    return 0;
+}
